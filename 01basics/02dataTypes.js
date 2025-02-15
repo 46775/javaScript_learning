37eda6a365d63669d87804a63b15100169421b2f
Sujal Kawale <sujal@kali>
2025-02-16 00:37:51 +0530

    data types added

---
+++ 01basics/02dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treats all js code as newer version
+
+// alert(3 + 3); we are using node so we can't use alert, but it is used in browser
+
+// Data types in JS
+let Name= "sujal"; //string
+let age = 21; //number
+let isApproved = true; //boolean
+let firstName = undefined; //undefined 
+let lastName = null; //null standalone value
+
+//object
+// typeof is used to check the type of variable
+console.log(typeof Name);
+
+
