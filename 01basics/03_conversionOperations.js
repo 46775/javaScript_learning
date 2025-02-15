111771c58971570c2dafa14a3272ec9203a437a9
Sujal Kawale <sujal@kali>
2025-02-16 00:58:27 +0530

    conversion examples

---
+++ 01basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = 12
+
+// Convert score to string
+let scoreAsString = String(score)
+console.log(scoreAsString);
+console.log(typeof scoreAsString);
+
+// Convert score to boolean
+let scoreAsBoolean = Boolean(score)
+console.log(scoreAsBoolean);
+console.log(typeof scoreAsBoolean);
+
+// Convert score to number
+let scoreAsNumber = Number(score)
+console.log(scoreAsNumber);
+console.log(typeof scoreAsNumber);
+
