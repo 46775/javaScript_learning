ea90182c606ceebc7113f736b567071ff70c31f6
Sujal Kawale <sujal@kali>
2025-02-17 00:36:09 +0530

    notes file added

--- 01basics/03_conversionOperations.js
+++ 01basics/03_conversionOperations.js
@@ -18,3 +18,4 @@ console.log(typeof scoreAsNumber);
 
 
 
+
--- 01basics/test.js
+++ 01basics/test.js
@@ -1,1 +1,7 @@
 console.log("hello world sujal")
+
+
+let id = Symbol("123");
+let id1 = Symbol("123");
+console.log(id === id1); // false
+console.log(typeof id, id1 )
